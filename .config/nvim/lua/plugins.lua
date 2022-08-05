627640fca6fefae5b2925b7ba4e16dd23c9a8975
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-08-05 20:27:25 +0100

    Add files via upload

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,10 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim' -- Packer can manage itself
         -- ColorScheme
@@ -17,6 +24,8 @@ return require('packer').startup(function(use)
     use 'mattn/emmet-vim'
     use 'sheerun/vim-polyglot'
     use 'windwp/nvim-autopairs'
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
-
 
