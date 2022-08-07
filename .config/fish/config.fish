292796d93a151b4a6db5059237a60bde746f419c
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-08-07 22:44:03 +0100

    Add files via upload

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -65,10 +65,10 @@ alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 
 # Changing "ls" to "exa"
-alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+alias ls='exa -l --color=always --group-directories-first'  # my preferred listing
 alias la='exa -a --color=always --group-directories-first'  # all files and dirs
 alias ll='exa -l --color=always --group-directories-first'  # long format
-alias lt='exa -aT --color=always --group-directories-first' # tree listing
+alias lt='exa -T --color=always --group-directories-first' # tree listing
 alias l.='exa -a | egrep "^\."'
 
 ## confirm before overwriting something
