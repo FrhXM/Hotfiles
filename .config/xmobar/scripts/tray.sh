1b67f55e6ac06b4b1bb3d84a40a02b87467304e2
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-08-28 15:36:34 +0100

    Add files via upload

--- .config/xmobar/scripts/tray.sh
+++ .config/xmobar/scripts/tray.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
 killall trayer
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --height 30 --distance 3 --width 250 --margin 16 --transparent true --alpha 20 --tint 0x11121D --widthtype request 
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true \
+       --height 30 --distance 3 --width 250 --margin 16 --alpha 20 --tint 0x11121D \
+       --transparent true --widthtype request 
 
