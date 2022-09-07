eed877ce02cd2ebeea960d9e07ddbf0c484a3472
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-09-07 21:41:56 +0100

    Add files via upload

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -215,7 +215,7 @@ projects =
 -- Startup Hooks
 ------------------------------------------------------------------------
 myStartupHook = do
-    spawnOnce "xwallpaper --zoom ~/pix/wall/lookup.jpg"                             -- Wallpapers
+    spawnOnce "xwallpaper --zoom ~/pix/wall/myGirl.jpg"                             -- Wallpapers
     spawnOnce "dunst"                                                               -- notfiction
     spawnOnce "unclutter"                                                           -- hidden Mouse
     spawnOnce "nm-applet"                                                           -- networkManager-applte {systemTray}
