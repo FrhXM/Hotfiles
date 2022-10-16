5fd424d45a17a9c208a907c9bd627fe0b75e071f
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-10-16 15:01:03 +0100

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,23 @@
-require("plugs")
-require("keys")
-require("opts")
-require("set")
+--[[
+
+Neovim init file
+Maintainer: brainf+ck
+Website: https://github.com/brainfucksec/neovim-lua
+
+--]]
+
+-- Import Lua modules
+require('packer_init')
+require('core/options')
+require('core/autocmds')
+require('core/keymaps')
+require('core/colors')
+require('core/statusline')
+-- require('lsp/lspconfig')
+require('plugins/others')
+require('plugins/nvim-tree')
+require('plugins/indent-blankline')
+require('plugins/nvim-cmp')
+require('plugins/nvim-treesitter')
+require('plugins/alpha-nvim')
+
