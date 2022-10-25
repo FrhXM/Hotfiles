ae1aba14eadf2e053cda13b08448beb938fa6eb4
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-10-25 12:11:11 +0100

    Add files via upload

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -2,6 +2,14 @@
 -- Define keymaps of Neovim and installed plugins.
 -----------------------------------------------------------
 
+-- Change leader to space
+vim.g.mapleader = ' '
+
+-- Change leader to a comma
+-- vim.g.mapleader = ','
+
+local map = vim.api.nvim_set_keymap
+
 local function map(mode, lhs, rhs, opts)
   local options = { noremap = true, silent = true }
   if opts then
@@ -9,9 +17,6 @@ local function map(mode, lhs, rhs, opts)
   end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
-
--- Change leader to a comma
--- vim.g.mapleader = ','
 
 -----------------------------------------------------------
 -- Neovim shortcuts
@@ -23,8 +28,9 @@ map('', '<down>', '<nop>')
 map('', '<left>', '<nop>')
 map('', '<right>', '<nop>')
 
--- Map Esc to kk
+-- Map Esc to kk && jj
 map('i', 'kk', '<Esc>')
+map('i', 'jj', '<Esc>')
 
 -- Clear search highlighting with <leader> and c
 map('n', '<leader>c', ':nohl<CR>')
@@ -33,15 +39,17 @@ map('n', '<leader>c', ':nohl<CR>')
 map('n', '<F2>', ':set invpaste paste?<CR>')
 vim.opt.pastetoggle = '<F2>'
 
--- Change split orientation
-map('n', '<leader>tk', '<C-w>t<C-w>K') -- change vertical to horizontal
-map('n', '<leader>th', '<C-w>t<C-w>H') -- change horizontal to vertical
-
 -- Move around splits using Ctrl + {h,j,k,l}
 map('n', '<C-h>', '<C-w>h')
 map('n', '<C-j>', '<C-w>j')
 map('n', '<C-k>', '<C-w>k')
 map('n', '<C-l>', '<C-w>l')
+
+-- Moving Between Buffers
+map('n', '<C-h>', '<C-w>h', { noremap = true, silent = false })
+map('n', '<C-l>', '<C-w>l', { noremap = true, silent = false })
+map('n', '<C-j>', '<C-w>j', { noremap = true, silent = false })
+map('n', '<C-k>', '<C-w>k', { noremap = true, silent = false })
 
 -- Reload configuration without restart nvim
 map('n', '<leader>r', ':so %<CR>')
