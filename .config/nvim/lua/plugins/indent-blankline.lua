2bc97d03dda2a3b96cd3a31c360a5e2fa9bd28a6
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-10-25 12:12:07 +0100

    Add files via upload

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -5,7 +5,6 @@
 -- Plugin: indent-blankline
 -- url: https://github.com/lukas-reineke/indent-blankline.nvim
 
-
 local status_ok, indent_blankline = pcall(require, 'indent_blankline')
 if not status_ok then
   return
