6597d68f3af68c8856ffc1269498f370c7515f9b
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-11-13 11:36:15 +0100

    Add files via upload

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -42,8 +42,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<Tab>", ":bnext<CR>", opts)
+keymap("n", "<S-Tab>", ":bprevious<CR>", opts)
 
 -- Clear search highlighting with <leader> and c
 keymap('n', '<leader>c', ':nohl<CR>', opts)
