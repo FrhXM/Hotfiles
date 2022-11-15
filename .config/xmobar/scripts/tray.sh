325e253ea3da8a35985f79c3dc3cfde819bd0ac8
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-11-15 12:09:25 +0100

    Delete tray.sh

--- .config/xmobar/scripts/tray.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-killall trayer
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true \
-       --height 30 --distance 3 --width 250 --margin 16 --alpha 20 --tint 0x11121D \
-       --transparent true --widthtype request 
