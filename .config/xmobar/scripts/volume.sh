afd0070de1236d2b0290a96c75b9ee907847156f
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-11-15 12:40:25 +0100

    Add files via upload

--- .config/xmobar/scripts/volume.sh
+++ .config/xmobar/scripts/volume.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-dunstify \
+dunstify -t 300 \
   "Volume: $(printf "%03d%%" $(pamixer --get-volume))" \
-  -i volume-level-high \
+  -i "/usr/share/icons/Papirus/16x16/devices/audio-card.svg" \
   -r 28223 \
   -h int:value:$(pamixer --get-volume)
-
