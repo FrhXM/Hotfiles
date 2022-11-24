f2436c50cb7ff61b3e0a9ca8bb4c696c4cfa2318
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-11-24 16:52:14 +0100

    Add files via upload

--- .config/nvim/lua/packer_init.lua
+++ .config/nvim/lua/packer_init.lua
@@ -1,4 +1,13 @@
-require("packer").startup(function()
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+print("install_path: " .. install_path)
+if fn.empty(fn.glob(install_path)) > 0 then
+    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    download_result = fn.system({'ls', '-l', install_path})
+    print("download_result: " .. download_result)
+end
+
+return require("packer").startup(function()
     use("wbthomason/packer.nvim")
 
     -- LSP
@@ -45,4 +54,9 @@ require("packer").startup(function()
     use("nvim-telescope/telescope.nvim")
     use("ggandor/leap.nvim")
     use("akinsho/bufferline.nvim")
+
+ if packer_bootstrap then
+   require('packer').sync()
+ end
 end)
+
