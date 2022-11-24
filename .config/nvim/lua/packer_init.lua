509d25bfb6c0ccad7a3327e469b3748028af8803
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-11-24 16:22:11 +0100

    Add files via upload

--- .config/nvim/lua/packer_init.lua
+++ .config/nvim/lua/packer_init.lua
@@ -1,79 +1,48 @@
------------------------------------------------------------
--- Bootstrapping
--- Automatically install packer
------------------------------------------------------------
--- Automatically install packer
-local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+require("packer").startup(function()
+    use("wbthomason/packer.nvim")
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
-    'git',
-    'clone',
-    '--depth',
-    '1',
-    'https://github.com/wbthomason/packer.nvim',
-    install_path
-  })
-  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
-end
+    -- LSP
+    -- use("neovim/nvim-lspconfig")
+    -- use("hrsh7th/cmp-nvim-lsp")
+    -- use("hrsh7th/cmp-buffer")
+    -- use("hrsh7th/cmp-path")
+    -- use("hrsh7th/cmp-cmdline")
+    -- use("hrsh7th/nvim-cmp")
+    -- use("onsails/lspkind-nvim")
+    -- use("nvim-lua/lsp_extensions.nvim")
+    -- use("glepnir/lspsaga.nvim")
+    -- use("simrat39/symbols-outline.nvim")
 
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, 'packer')
-if not status_ok then
-  return
-end
-------------------------------------------------------------------------------
--- Autocommand that reloads neovim whenever you save the packer_init.lua file
-------------------------------------------------------------------------------
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost packer_init.lua source <afile> | PackerSync
-  augroup end
-]]
------------------------------------------------------------
--- Plugin manager configuration file
------------------------------------------------------------
--- Install plugins
-return packer.startup(function(use)
-  -- Add you plugins here:
-  use 'wbthomason/packer.nvim' -- packer can manage itself
+    -- easier coding
+    use("ryanoasis/vim-devicons")
+    use("nvim-tree/nvim-web-devicons")
+    use("goolord/alpha-nvim")
+    use("feline-nvim/feline.nvim")
+    use("norcalli/nvim-colorizer.lua")
+    use("lukas-reineke/indent-blankline.nvim")
+    use("akinsho/toggleterm.nvim")
+    use("windwp/nvim-autopairs")
 
-  -- File explorer
-  use 'kyazdani42/nvim-tree.lua'
+    -- Vim Plugins
+    use("tpope/vim-surround")
+    use("tpope/vim-commentary")
+    use("mattn/emmet-vim")
+    use("alvan/vim-closetag")
+    use("sheerun/vim-polyglot")
 
-  -- Indent line
-  use 'lukas-reineke/indent-blankline.nvim'
+   -- Color theme
+    use("xiyaowong/nvim-transparent")
+    use("catppuccin/nvim")
+    use("folke/tokyonight.nvim")
+    use("Everblush/everblush.nvim")
+    use({ 'tiagovla/tokyodark.nvim',
+        config = function()
+        vim.cmd("colorscheme tokyodark")
+        end })
 
-  -- Autopair
-  use { 'windwp/nvim-autopairs', config = function()
-      require('nvim-autopairs').setup{}
-    end
-  }
-
-  -- Icons
-  use 'kyazdani42/nvim-web-devicons'
-
-  -- Tag viewer
-  use 'preservim/tagbar'
-
-  -- Color schemes
-  use 'xiyaowong/nvim-transparent'
-  use 'navarasu/onedark.nvim'
-  use 'tiagovla/tokyodark.nvim'
-  use 'tanvirtin/monokai.nvim'
-
-  -- Statusline
-  use { 'feline-nvim/feline.nvim', requires = { 'kyazdani42/nvim-web-devicons' }, }
-
-  -- Dashboard (start screen)
-  use { 'goolord/alpha-nvim', requires = { 'kyazdani42/nvim-web-devicons' }, }
-
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+    -- navigation
+    use("kyazdani42/nvim-tree.lua")
+    use("nvim-telescope/telescope.nvim")
+    use("ggandor/leap.nvim")
+    use("akinsho/bufferline.nvim")
 end)
